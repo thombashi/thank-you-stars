9dbeec873839a0472735dfdc38eeea8bcb0eb199
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-04-02 22:43:41 +0900

    Change the entry point for test cases

--- test/test_cli.py
+++ test/test_cli.py
@@ -8,7 +8,7 @@ import pytest
 from subprocrunner import SubprocessRunner
 
 
-TYS_CMD = "thank-you-stars"
+TYS_CMD = ["python", "-m", "thank_you_stars"]
 CHK_OPT = "--check"
 DEPTH_OPT = "--depth"
 DRY_RUN_OPT = "--dry-run"
@@ -18,10 +18,10 @@ class Test_tys:
     @pytest.mark.parametrize(
         ["command", "expected"],
         [
-            [[TYS_CMD, "pytablewriter", DRY_RUN_OPT], 0],
-            [[TYS_CMD, "thank-you-stars", CHK_OPT], 0],
-            [[TYS_CMD, "thank-you-stars", CHK_OPT, "--include-owner-repo", DEPTH_OPT, 3], 0],
-            [[TYS_CMD, "pytablewriter", DRY_RUN_OPT, DEPTH_OPT, -1], errno.EINVAL],
+            [TYS_CMD + ["pytablewriter", DRY_RUN_OPT], 0],
+            [TYS_CMD + ["thank-you-stars", CHK_OPT], 0],
+            [TYS_CMD + ["thank-you-stars", CHK_OPT, "--include-owner-repo", DEPTH_OPT, 3], 0],
+            [TYS_CMD + ["pytablewriter", DRY_RUN_OPT, DEPTH_OPT, -1], errno.EINVAL],
         ],
     )
     def test_normal(self, command, expected):
